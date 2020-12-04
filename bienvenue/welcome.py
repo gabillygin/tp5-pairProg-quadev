92ec66898b29504c691ba4dfdc149ee2d1bde238
Gabriel Chastaing <gabriel.chastaing@gmail.com>
2020-12-04 11:38:01 +0100

    Features 10 to 14 added

--- bienvenue/welcome.py
+++ bienvenue/welcome.py
@@ -1,25 +1,97 @@
-def hello_prenom(input):
+def supprime_prenom_non_affiche(input):
+    for prenom in input:
+        if prenom[0] == '!':
+            for check in input:
+                if prenom[1:].strip() == check.strip():
+                    del input[input.index(check)]
+            del input[input.index(prenom)]
+    return input
+
+
+def compte_occ_prenom_min_maj(input):
+    nb_prenom_min = []
+    nb_prenom_maj = []
+    for prenom in input:
+        if(prenom.isupper()):
+            nb_prenom_maj.append(input.count(prenom))
+            nb_prenom_min.append(0)
+        else:
+            nb_prenom_min.append(input.count(prenom))
+            nb_prenom_maj.append(0)
+        for check in input[input.index(prenom) + 1:]:
+            if prenom.strip() == check.strip():
+                del input[input.index(check)]
+    return nb_prenom_min,nb_prenom_maj
+
+
+def traitement_input(input):
     input = input.split(',')
     sortie = "Hello"
     sortie_maj = "HELLO"
+
+    if input[0] == '':
+        sortie += ', my friend'
+        return sortie, sortie_maj
+
+    input = supprime_prenom_non_affiche(input)
+
+    if input == []:
+        sortie += ', my friend'
+        return sortie, sortie_maj
+
+    nb_prenom_min, nb_prenom_maj = compte_occ_prenom_min_maj(input)
+
+    if sum(nb_prenom_min) >= 5:
+        sortie += ', world'
+        print(sortie,' ',input)
+        if contient_yoda(input) == 1:
+            print("oui")
+            sortie = sortie[7:]
+            sortie = sortie.capitalize()
+            sortie += ", Hello"
+        return sortie, sortie_maj
+    elif sum(nb_prenom_maj) >= 5:
+        sortie_maj += ', WORLD'
+        return sortie, sortie_maj
+
     for prenom in input:
         if len(prenom.strip()) == 0:
-            prenom = "my friend"
-            sortie += ', ' + prenom
+            sortie += ', my friend'
+            return sortie, sortie_maj
+        if prenom.isupper():
+            prenom = prenom.strip()
+            sortie_maj += ', ' + prenom
         else:
-            if prenom.isupper():
-                sortie_maj += ', ' + prenom
-            else:
-                prenom = prenom.capitalize()
-                if prenom == input[-1]:
-                    sortie += ' and ' + prenom
+            if prenom == input[-1] and len(input) != 1:
+                if nb_prenom_maj[input.index(prenom)] > 1:
+                    prenom = prenom.strip()
+                    sortie += ' and ' + prenom.capitalize()
                 else:
-                    sortie += ', ' + prenom
-    return (sortie,sortie_maj)
+                    prenom = prenom.strip()
+                    sortie += ' and ' + prenom.capitalize()
+            else:
+                if nb_prenom_min[input.index(prenom)] > 1:
+                    prenom = prenom.strip()
+                    sortie += ', ' + prenom.capitalize() + '(x' + str(nb_prenom_min[input.index(prenom)]) + ')'
+                else:
+                    prenom = prenom.strip()
+                    sortie += ', ' + prenom.capitalize()
+
+    if contient_yoda(sortie)==1:
+        sortie = sortie[7:]
+        sortie += ", Hello"
+
+    return [sortie, sortie_maj]
+
+
+def contient_yoda(input):
+    if 'yoda' in input or 'Yoda' in input:
+        return 1
+    return 0
 
 
 def sortie_bienvenue(input):
-    sortie = hello_prenom(input)
+    sortie = traitement_input(input)
     if sortie[1] != "HELLO":
         if sortie[0] != "Hello" and sortie[0] != "Hello, my friend":
             return sortie[0] + '. AND ' + sortie[1] + ' !'
--- test/test_bienvenue.py
+++ test/test_bienvenue.py
@@ -1,27 +1,56 @@
-def test_hello_prenom_normal():
+def test_sortie_bienvenue_normal():
     from bienvenue.welcome import sortie_bienvenue as hp
     assert hp('gabriel') == "Hello, Gabriel"
     assert hp('   ') == 'Hello, my friend'
-    assert hp('') == 'Hello, my friend'
+    assert hp('gabriel  , Nicolas  ') == "Hello, Gabriel and Nicolas"
 
 
-def test_hello_prenom_empty():
+def test_sortie_bienvenue_empty():
     from bienvenue.welcome import sortie_bienvenue as hp
     assert hp('   ') == 'Hello, my friend'
     assert hp('') == 'Hello, my friend'
 
 
-def test_hello_prenom_maj():
+def test_sortie_bienvenue_maj():
     from bienvenue.welcome import sortie_bienvenue as hp
     assert hp('NICOLAS') == "HELLO, NICOLAS!"
 
 
-def test_hello_prenom_normal_maj():
+def test_sortie_bienvenue_normal_maj():
     from bienvenue.welcome import sortie_bienvenue as hp
     assert hp('gabriel,NICOLAS') == "Hello, Gabriel. AND HELLO, NICOLAS !"
     #ISSUE / ! \
     #assert hp('gabriel, nicolas,NICOLAS') == "Hello, Gabriel, Nicolas. AND HELLO, NICOLAS !"
 
-def test_hello_prenom_and():
+def test_sortie_bienvenue_and():
     from bienvenue.welcome import sortie_bienvenue as hp
     assert hp('gabriel,Nicolas') == "Hello, Gabriel and Nicolas"
+
+def test_sortie_bienvenue_non_affiche():
+    from bienvenue.welcome import sortie_bienvenue as hp
+    assert hp('!gabriel, gabriel, nicolas') == "Hello, Nicolas"
+    assert hp('!gabriel,gabriel') == "Hello, my friend"
+    assert hp('!gabriel') == "Hello, my friend"
+
+
+def test_sortie_bienvenue_multiple():
+    from bienvenue.welcome import sortie_bienvenue as hp
+    assert hp('gabriel,gabriel') == "Hello, Gabriel(x2)"
+
+
+def test_sortie_bienvenue_world_min():
+    from bienvenue.welcome import sortie_bienvenue as hp
+    assert hp('gabriel,tony,denis,france,nina,robinson') == "Hello, world"
+
+
+def test_sortie_bienvenue_world_maj():
+    from bienvenue.welcome import sortie_bienvenue as hp
+    assert hp('GABRIEL,TONY,DENIS,FRANCE,NONE,ROBINSON') == "HELLO, WORLD!"
+
+
+def test_sortie_bienvenue_yoda():
+    from bienvenue.welcome import sortie_bienvenue as hp
+    assert hp('gabriel,yoda') == "Gabriel and Yoda, Hello"
+    assert hp('yoda') == "Yoda, Hello"
+    assert hp('yoda,gabriel,nicolas,denis,françouise,papy') == "World, Hello"
+
